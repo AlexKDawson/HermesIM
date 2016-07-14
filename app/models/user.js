6a6ea472c728137d126cae3f62cb8132cf9f8e90
akd40 <akd40@pitt.edu>
2016-07-14 16:55:04 -0400

    add login sessions and static names in messages

--- app/models/user.js
+++ app/models/user.js
@@ -21,7 +21,7 @@ userSchema.methods.generateHash = function(password) {
 
 // checking if password is valid
 userSchema.methods.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.local.password);
+  return bcrypt.compareSync(password, this.password);
 };
 
 var user = authdb.model('user', userSchema);
--- app/routes.js
+++ app/routes.js
@@ -7,13 +7,22 @@ module.exports = function(app, passport){
 
   app.get('/', function(req, res){
     retrieve.output(function(data){
-      res.render(__dirname + '/views/home.ejs', {msgs: data});
+      res.render(__dirname + '/views/home.ejs');
     });
   });
 
-  app.post('/', function(req, res){
+  app.get('/messaging', isLoggedIn, function(req, res){
+    retrieve.output(function(data){
+      res.render(__dirname + '/views/messaging.ejs', {
+        msgs: data,
+        user: req.user
+      });
+    });
+  });
+
+  app.post('/messaging', function(req, res){
     send.msg(req, res);
-    res.redirect('/'); //use a callback
+    res.redirect('/messaging'); //use a callback
   });
 
   app.get('/signup', function(req, res) {
@@ -26,4 +35,26 @@ module.exports = function(app, passport){
     failureRedirect : '/signup', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
+
+  app.get('/login', function(req, res) {
+    // render the page and pass in any flash data if it exists
+    res.render(__dirname + '/views/login.ejs', { message: req.flash('loginMessage') });
+  });
+
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/messaging', // redirect to the secure profile section
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
 };
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+  // if user is authenticated in the session, carry on
+  if (req.isAuthenticated())
+    return next();
+  // if they aren't redirect them to the home page
+  res.redirect('/');
+}
